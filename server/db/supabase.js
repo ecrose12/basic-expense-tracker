7d586e9be6c42b37286e0ed28ac531442554dd03
ecrose12 <elizabeth.crose12@gmail.com>
2026-07-18 17:58:04 -0500

    7/18/26

--- server/db/supabase.js
+++ server/db/supabase.js
@@ -1,9 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY // server = service key
+  process.env.SUPABASE_PUBLISHABLE_KEY
 );
-
 module.exports = supabase;
